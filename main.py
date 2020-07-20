d738f7caaa7163e1610f10fee66e2b5649844508
Erick Quintanilla <equintanilla@berkeley.edu>
2020-07-20 03:20:16 -0700

    Update main

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger
 
 def extract_toc_from_pdf(pdf, page_begin, page_end):
     pdf_object = PdfFileReader(pdf)
-    for i in range(int(page_begin), int(page_end) + 1):
+    for i in range(page_begin, page_end + 1):
         transform_toc_to_txt(pdf_object.getPage(i))
 
 def transform_toc_to_txt(pdf_toc_page):
     text_file = open("toc.txt", "a")
-    text_file.write(pdf_toc_page.extractText())
+    text_file.write(pdf_toc_page.extractText() + "\n")
 
 def pdf_toc_to_txt(arg_list):
     pdf = arg_list[0]
-    toc_page_begin = arg_list[1]
-    toc_page_end = arg_list[2]
+    toc_page_begin = int(arg_list[1])
+    toc_page_end = int(arg_list[2])
     extract_toc_from_pdf(pdf, toc_page_begin, toc_page_end)
 
 if __name__ == '__main__':
