cded6f8f55989748abd1733981e7da4bdfe0a15d
Erick Quintanilla <equintanilla@berkeley.edu>
2020-08-10 11:21:40 -0700

    Add feature to create a seperate PDF out of the table of contents

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger
 def extract_toc_from_pdf(pdf, page_begin, page_end):
     pdf_object = PdfFileReader(pdf)
     for i in range(page_begin, page_end + 1):
-        transform_toc_to_txt(pdf_object.getPage(i))
+        create_pdf_from_toc(pdf_object.getPage(i))
+        # transform_toc_to_txt(pdf_object.getPage(i))
+
+def create_pdf_from_toc(pdf_obj):
+    pdf_merger = PdfFileMerger()
+    for path in pdf_obj:
+        pdf_merger.append(path)
+    with open("toc.pdf", 'wb') as fileobj:
+        pdf_merger.write(fileobj)
+
 
 def transform_toc_to_txt(pdf_toc_page):
     text_file = open("toc.txt", "a")
     text_file.write(pdf_toc_page.extractText() + "\n")
 
-def pdf_toc_to_txt(arg_list):
+def pdf_toc(arg_list):
     pdf = arg_list[0]
     toc_page_begin = int(arg_list[1])
     toc_page_end = int(arg_list[2])
@@ -21,4 +30,4 @@ if __name__ == '__main__':
     cmnd_line_arg_list = []
     for arg in sys.argv[1:]:
         cmnd_line_arg_list.append(arg)
-    pdf_toc_to_txt(cmnd_line_arg_list)
+    pdf_toc(cmnd_line_arg_list)
