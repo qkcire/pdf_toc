d98bee9ace9631b9d189f93f6fe99877793a5a59
Erick Quintanilla <equintanilla@berkeley.edu>
2020-08-10 12:30:19 -0700

    Refactor extract_toc_from_pdf method to create a toc.pdf, create toc.txt, and remove unwanted items directory

--- main.py
+++ main.py
@@ -3,10 +3,31 @@ import sys
 from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger
 
 def extract_toc_from_pdf(pdf, page_begin, page_end):
-    pdf_object = PdfFileReader(pdf)
-    for i in range(page_begin, page_end + 1):
-        transform_toc_to_txt(pdf_object.getPage(i))
-        
+    toc_paths = []
+    # split pdf into individual pages containing toc
+    pdf_read_obj = PdfFileReader(pdf)
+    for page in range(page_begin, page_end + 1):
+        pdf_writer = PdfFileWriter()
+        pdf_writer.addPage(pdf_read_obj.getPage(page))
+        toc_pdf_filename = 'toc_page_{}.pdf'.format(page)
+        toc_paths.append(toc_pdf_filename)
+        with open(toc_pdf_filename, 'wb') as out:
+            pdf_writer.write(out)
+    # merge individual pages into a seperate toc.pdf
+    pdf_merge_obj = PdfFileMerger()
+    for path in toc_paths:
+        pdf_merge_obj.append(path)
+    with open('toc.pdf', 'wb') as out:
+        pdf_merge_obj.write(out)
+    # turn toc.pdf into toc.txt
+    toc_pdf_read_obj = PdfFileReader('toc.pdf')
+    for i in range(toc_pdf_read_obj.getNumPages()):
+        transform_toc_to_txt(toc_pdf_read_obj.getPage(i))
+    # clean up / remove individual pages from directory
+    for path in toc_paths:
+        cmnd = "rm {}".format(path)
+        os.system(cmnd)
+
 def transform_toc_to_txt(pdf_toc_page):
     text_file = open("toc.txt", "a")
     text_file.write(pdf_toc_page.extractText() + "\n")
